5387f8aa46e5e65f16b54f5f4009bf872b0b956b
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-17 13:39:52 +0100

    added main function and ability to change for given folder all files

--- rename.py
+++ rename.py
@@ -11,9 +11,27 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 file_list = os.listdir(PATH)
 print(file_list)
 
+subfolder="\\test_dir1"
+
 initial_name = 'test_file.txt'
 final_name = 'test_file2.txt'
 
 
 
-os.rename(r'C:\Users\user\Python_user\Ada_rename_videos\test_file.txt', r'C:\Users\user\Python_user\Ada_rename_videos\test_file2.txt')
+#os.rename(r'C:\Users\user\Python_user\Ada_rename_videos\test_file.txt', r'C:\Users\user\Python_user\Ada_rename_videos\test_file2.txt')
+
+def main(): 
+    i = 0
+      
+    for filename in os.listdir(PATH + subfolder): 
+        dst = "\\test" + str(i) + ".txt"
+        src = "\\" + filename 
+ 
+        os.rename(PATH + subfolder+src, PATH + subfolder+dst) 
+        i += 1
+  
+# Driver Code 
+if __name__ == '__main__': 
+      
+    # Calling main() function 
+    main() 
