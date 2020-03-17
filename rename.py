c9f8c3ba11149be51e708ad077b2a069f3863e0b
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-17 13:20:33 +0100

    created first rename

--- rename.py
+++ rename.py
@@ -6,3 +6,4 @@ Created on Tue Mar 17 13:15:29 2020
 """
 
 import os
+os.rename(r'C:\Users\user\Python_user\Ada_rename_videos\test_file.txt', r'C:\Users\user\Python_user\Ada_rename_videos\test_file2.txt')
