aa6447e0385cdc41add268cfe2f849e31f63a0ad
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-17 13:27:10 +0100

    Added file list, path and initial and finale name

--- rename.py
+++ rename.py
@@ -6,4 +6,14 @@ Created on Tue Mar 17 13:15:29 2020
 """
 
 import os
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+file_list = os.listdir(PATH)
+print(file_list)
+
+initial_name = 'test_file.txt'
+final_name = 'test_file2.txt'
+
+
+
 os.rename(r'C:\Users\user\Python_user\Ada_rename_videos\test_file.txt', r'C:\Users\user\Python_user\Ada_rename_videos\test_file2.txt')
