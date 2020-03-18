927ca77c8a647f953df83ec071b2874a4f9667c6
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-18 10:13:28 +0100

    name specification according to client wishes

--- rename.py
+++ rename.py
@@ -12,22 +12,30 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 file_list = os.listdir(PATH)
 print(file_list)
 
-subfolder="\\"+"test_dir1"
+subfolder="test_dir1"
 
 
 def main(): 
     i = 0
+    m=0
     list_before = []
     list_after = []
+    kierunki=["W","E","S","N"]
+    myszy = [1,3,4,5,6]
+    
+
       
-    for filename in os.listdir(PATH + subfolder): 
+    for filename in os.listdir(PATH + "\\"+ subfolder): 
         list_before.append(filename)
-        dst = subfolder + "_mysza"+str(i)+ "_P" + str(i%4+1) + "_"+ filename
+
+        dst = "\\reference_L1_3mies_M"+str(myszy[m])+ "_P" + str(i%4+1) + "_"+ str(kierunki[i%4]) + "_"+ subfolder + ".txt"
         src = "\\" + filename 
  
-        os.rename(PATH + subfolder+src, PATH + subfolder+dst) 
-        i += 1
+        os.rename(PATH + "\\"+ subfolder+src, PATH + "\\"+ subfolder+dst) 
         list_after.append(dst)
+        i += 1
+        if i%4 == 0:
+            m += 1
     print(list_before, list_after)
     
     result_file = open("zamiana.csv", "w")
