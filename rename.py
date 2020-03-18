b084626d6329a62fe2635ed4f2f69b6bc782fc52
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-18 10:35:31 +0100

    everything works now according to sepcification

--- rename.py
+++ rename.py
@@ -12,7 +12,10 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 file_list = os.listdir(PATH)
 print(file_list)
 
-subfolder="test_dir1"
+subfolder="03.12.2018"
+kierunki=["W","E","S","N"]
+myszy = [1,3,4,5,6,7,8,9,10,11,12,14,15,18,19,20,21,23,24,25,27,29,30,31,32,38,67,89,99]
+fileextention = ".mpg"
 
 
 def main(): 
@@ -20,15 +23,15 @@ def main():
     m=0
     list_before = []
     list_after = []
-    kierunki=["W","E","S","N"]
-    myszy = [1,3,4,5,6]
-    
 
+    
+    result_file = open("zamiana.csv", "w")
+    wr = csv.writer(result_file, dialect='excel')
       
     for filename in os.listdir(PATH + "\\"+ subfolder): 
         list_before.append(filename)
 
-        dst = "\\reference_L1_3mies_M"+str(myszy[m])+ "_P" + str(i%4+1) + "_"+ str(kierunki[i%4]) + "_"+ subfolder + ".txt"
+        dst = "\\reference_L1_3mies_M"+str(myszy[m])+ "_P" + str(i%4+1) + "_"+ str(kierunki[i%4]) + "_"+ subfolder + fileextention
         src = "\\" + filename 
  
         os.rename(PATH + "\\"+ subfolder+src, PATH + "\\"+ subfolder+dst) 
@@ -38,9 +41,11 @@ def main():
             m += 1
     print(list_before, list_after)
     
-    result_file = open("zamiana.csv", "w")
-    for j in range(len(list_before)):
-        result_file.write(list_before[j] + ", " + list_after[j] + "\n")
+    # 
+    # for j in range(len(list_before)):
+    #     result_file.writerow(list_before[j] + ", " + list_after[j] + "\n")
+    wr.writerow(list_before)
+    wr.writerow(list_after)
     result_file.close()
 
   
